cd21f4106b2d1fa9bb18f4aa587cc91b7e2e9d66
Nightfall1988 <grigorijs.mamilovs@gmail.com>
2021-02-20 19:19:50 +0200

    Update 01.php

--- basics/01.php
+++ basics/01.php
@@ -1,16 +1,3 @@
-
-###### Exercise 1
-
-Create variable that prints out an integer 10, float 10.10, string "hello world"
-
-###### Exercise 2
-
-Dump the same values that should display both data type & its value. (Note, usage of var_dump)
-
-###### Exercise 3
-
-Concatenate your name, surname and integer of your age.
-
 <?php
 
 // 01
